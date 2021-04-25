e21254a38bbb1e0457562fc2eacd5365e6fc152d
root <anshu98@outlook.com>
2021-04-25 20:22:15 +0530

    sortwithoutAlgo

--- sortwithoutAlgo.py
+++ sortwithoutAlgo.py
@@ -1,12 +1,16 @@
-a = []
-result = []
-for i in range(3):
-    a.append(int(input()))
-if a[0]<a[1] and a[0]<a[2]:
-    result.append(a[0])
-elif a[1]>a[0] and a[1]<a[2]:
-    result.append(a[1])
-elif a[2]>a[1] and a[2]>a[0]:
-    result.append(a[2])
-print(result)
-print(a)
+def sort012(arr,n):
+    key = {0:0,1:0,2:0}
+    for i in arr:
+        for j in key.keys():
+            if i==j:
+                key[j] += 1
+    a = []
+    for i in range(0,3):
+        k = key[i]
+        while k:
+            a.append(i)
+            k -= 1
+    return a
+n = 2
+arr = [0,2,1,2,0]
+print(*(sort012(arr,n)))

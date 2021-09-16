24b496fb15bd8addcf5f1ecc269444e77e536a73
abhishek <anshu98@outlook.com>
2021-09-16 23:45:44 +0530

    bubble_sort

--- bubble_sort.go
+++ bubble_sort.go
@@ -2,12 +2,20 @@ package main
 
 import "fmt"
 
+func swap(a *int, b *int) {
+	*a, *b = *b, *a
+}
 func bubblesort(arr []int) []int {
 	for i := 0; i < len(arr); i++ {
+		var swappped bool
 		for j := i + 1; j < len(arr); j++ {
 			if arr[i] > arr[j] {
-				arr[j], arr[i] = arr[i], arr[j]
+				swap(&arr[i], &arr[j])
+				swappped = true
 			}
+		}
+		if !swappped {
+			break
 		}
 	}
 	return arr

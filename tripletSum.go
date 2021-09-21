276ecf19cb9d7ea6c8665cb7a24afb1395ec408b
abhishek <anshu98@outlook.com>
2021-09-21 14:33:43 +0530

    tripleSumOptimized

--- tripletSum.go
+++ tripletSum.go
@@ -5,23 +5,37 @@ import (
 	"sort"
 )
 
-func TripletSumArray(arr []int, n, x int) int {
+//Naive Approach
+func TripletSumArrayOptimized(arr []int, n, x int) int {
 	sort.Slice(arr, func(a, b int) bool {
 		return arr[a] < arr[b]
 	})
-
 	for i := 0; i < n-2; i++ {
 		j := i + 1
 		k := n - 1
-		sum := x - arr[i]
 		for j < k {
-			if arr[j]+arr[k] == sum {
+			computed_sum := arr[i] + arr[j] + arr[k]
+			if computed_sum == x {
 				return 1
-			}
-			if arr[j]+arr[k] < sum {
+			} else if computed_sum < x {
 				j += 1
-			} else {
+			} else if computed_sum > x {
 				k -= 1
+			}
+		}
+	}
+	return 0
+
+}
+
+//Naive Approach
+func TripletSumArray(arr []int, n, x int) int {
+	for i := 0; i < n; i++ {
+		for j := i + 1; j < n; j++ {
+			for k := j + 1; k < n; k++ {
+				if arr[i]+arr[j]+arr[k] == x {
+					return 1
+				}
 			}
 		}
 	}
@@ -29,11 +43,11 @@ func TripletSumArray(arr []int, n, x int) int {
 }
 func main() {
 	array1 := []int{1, 4, 45, 6, 10, 8}
-	array2 := []int{1, 2, 4, 3, 6}
+	// array2 := []int{1, 2, 4, 3, 6}
 	n := 6
 	x := 13
 
 	fmt.Println(TripletSumArray(array1, n, x))
-	fmt.Println(TripletSumArray(array2, n, x))
+	fmt.Println(TripletSumArrayOptimized(array1, n, x))
 
 }

2be83744f89059eb83dd673d62874c6f8ddc6894
Abhishek Ratnam <jarvis@Abhisheks-MacBook-Air.local>
2022-03-23 12:53:36 +0530

    sockMerchant

--- pairs_counting.go
+++ pairs_counting.go
@@ -4,15 +4,6 @@ import (
 	"fmt"
 )
 
-/*
- * Complete the 'sockMerchant' function below.
- *
- * The function is expected to return an INTEGER.
- * The function accepts following parameters:
- *  1. INTEGER n
- *  2. INTEGER_ARRAY ar
- */
-
 func sockMerchant(n int32, ar []int32) int32 {
 	// Write your code here
 	pairs := map[int32]int32{}
